9fc3b257ecaf680aeaf923a0f2c169532c6aa159
N Akash <165415763+NimmalapudiAkash@users.noreply.github.com>
2024-09-23 23:18:51 +0530

    Update memu.py

--- CAFE Restro/memu.py
+++ CAFE Restro/memu.py
@@ -14,11 +14,11 @@ print("Menu:\nPizza: Rs40\nPasta: Rs50\nBurger: Rs60\nSalad: Rs70\nCoffee: Rs80"
 order_total = 0
 ordered_items = []
 
-# Take the first order
+
 item_1 = input("Enter the name of the item you want to order: ").lower()
 if item_1 in menu:
     order_total += menu[item_1]
-    ordered_items.append((item_1.capitalize(), menu[item_1]))  # Capitalizing for nice output
+    ordered_items.append((item_1.capitalize(), menu[item_1]))  
     print(f"Your item '{item_1}' has been added to your order.")
 else:
     print(f"Sorry, the item '{item_1}' is not available!")
@@ -29,7 +29,7 @@ if another_order == "yes":
     item_2 = input("Enter the name of the second item: ").lower()
     if item_2 in menu:
         order_total += menu[item_2]
-        ordered_items.append((item_2.capitalize(), menu[item_2]))  # Capitalizing for nice output
+        ordered_items.append((item_2.capitalize(), menu[item_2])) 
         print(f"Item '{item_2}' has been added to your order.")
     else:
         print(f"Sorry, the item '{item_2}' is not available!")
